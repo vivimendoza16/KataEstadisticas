37c0caa8f01f2b97f592c414464e6448ed03a3c0
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-25 09:29:56 -0500

    First Commit

---
+++ Estadisticas.py
@@ -0,0 +1,6 @@
+__author__ = 'cv.hernandez,vc.mendoza'
+
+class Calculos:
+
+    def Estadisticas (cadena):
+        pass
---
+++ EstadisticasTest.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+__author__ = 'cv.hernandez,vc.mendoza'
+
+class EstadisticasTest(TestCase):
+    def test_Estadisticas(self):
+        self.fail()
