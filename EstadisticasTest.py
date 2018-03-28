652793e94e7e3d7bd7c58e5f863b10a1cc0fe6c0
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 12:16:11 -0500

    Paso 10. Arreglo elementos, minimo y maximo vacio(Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -5,7 +5,7 @@ __author__ = 'cv.hernandez,vc.mendoza'
 
 class EstadisticasTest(TestCase):
     def test_Estadisticas(self):
-        self.assertEqual(Calculos().Estadisticas(""),[0,0], "arreglo elementos,minimo vacio")
+        self.assertEqual(Calculos().Estadisticas(""),[0,0,0], "arreglo elementos,minimo vacio")
 
     def test_Estadisticas_unNumero(self):
         self.assertEqual(Calculos().Estadisticas("5"),[1,5], "arreglo elementos,minimo un elemento")
