9ace33c6a8dde3c3870c62fb60d0b98d1aa57de1
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 12:47:20 -0500

    Paso 17. Arreglo elementos, minimo, maximo y promedio n numeros(Verde)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -8,11 +8,11 @@ class EstadisticasTest(TestCase):
         self.assertEqual(Calculos().Estadisticas(""),[0,0,0,0], "arreglo elementos,minimo,maximoy promedio vacio")
 
     def test_Estadisticas_unNumero(self):
-        self.assertEqual(Calculos().Estadisticas("5"),[1,5,5,5], "arreglo elementos,minimo,maximo un elemento")
+        self.assertEqual(Calculos().Estadisticas("5"),[1,5,5,5], "arreglo elementos,minimo,maximo, promedio un elemento")
 
     def test_Estadisticas_dosNumeros(self):
-        self.assertEqual(Calculos().Estadisticas("5,1"),[2,1,5,3], "arreglo elementos,minimo,maximo dos elementos")
+        self.assertEqual(Calculos().Estadisticas("5,1"),[2,1,5,3], "arreglo elementos,minimo,maximo,promedio dos elementos")
 
-    #def test_Estadisticas_Numneros(self):
-      #  self.assertEqual(Calculos().Estadisticas("5,2,11,6,3"),[5,2,11], "arreglo elementos,minimo N elementos")
+    def test_Estadisticas_Nnumeros(self):
+        self.assertEqual(Calculos().Estadisticas("5,3,11,7,4"),[5,3,11,6], "arreglo elementos,minimo, promedio N elementos")
 
