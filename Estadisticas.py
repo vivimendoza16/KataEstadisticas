9358abe6df5ad1d9d5066ec987b78e3f02efa074
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 12:14:46 -0500

    Paso 9. Arreglo elementos y minimo con N numeros(Verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -7,13 +7,9 @@ class Calculos:
             return ([0,0])
         elif "," in cadena:
             numeros=cadena.split(',')
-            print numeros
             numeros0=[int(i) for i in numeros]
-            print numeros0
-            numeros1=len(numeros0)
-            print numeros1
-            numeros2=min(numeros0)
-            print numeros2
-            return([numeros1,numeros2])
+            cantidad=len(numeros0)
+            minimo=min(numeros0)
+            return([cantidad,minimo])
         else:
             return([len(cadena),int(min(cadena))])
