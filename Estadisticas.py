2db11e61ef5b37fe45e18c7ff0f28d688eb250ff
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 12:13:03 -0500

    Paso 9. Arreglo elementos y minimo con N numeros(Amarillo)

--- Estadisticas.py
+++ Estadisticas.py
@@ -6,8 +6,14 @@ class Calculos:
         if cadena == "":
             return ([0,0])
         elif "," in cadena:
-            numeros1=len((int(cadena[0]),int(cadena[2])))
-            numeros2=min(int(cadena[0]),int(cadena[2]))
+            numeros=cadena.split(',')
+            print numeros
+            numeros0=[int(i) for i in numeros]
+            print numeros0
+            numeros1=len(numeros0)
+            print numeros1
+            numeros2=min(numeros0)
+            print numeros2
             return([numeros1,numeros2])
         else:
             return([len(cadena),int(min(cadena))])
