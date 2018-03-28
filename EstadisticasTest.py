1d59df470ebf20eb7e6a4b24246166937ef8254c
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 11:15:13 -0500

    Paso 1. Arreglo con elementos sin elementos(Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 
+from Estadisticas import Calculos
 __author__ = 'cv.hernandez,vc.mendoza'
 
 class EstadisticasTest(TestCase):
     def test_Estadisticas(self):
-        self.fail()
+        self.assertEqual(Calculos().Estadisticas(),[0], "arreglo vacio")
+
