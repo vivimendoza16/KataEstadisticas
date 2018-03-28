c72e57fa6bc1b5a7bc2ed4a61ba8a96ab1fdb626
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 12:32:11 -0500

    Paso 14. Arreglo elementos, minimo, maximo y promedio sin numeros(Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -5,7 +5,7 @@ __author__ = 'cv.hernandez,vc.mendoza'
 
 class EstadisticasTest(TestCase):
     def test_Estadisticas(self):
-        self.assertEqual(Calculos().Estadisticas(""),[0,0,0], "arreglo elementos,minimo,maximo vacio")
+        self.assertEqual(Calculos().Estadisticas(""),[0,0,0,0], "arreglo elementos,minimo,maximoy promedio vacio")
 
     def test_Estadisticas_unNumero(self):
         self.assertEqual(Calculos().Estadisticas("5"),[1,5,5], "arreglo elementos,minimo,maximo un elemento")
