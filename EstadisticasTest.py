8896fe542e086d93558785746cd50ff6479029f0
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 12:23:03 -0500

    Paso 12. Arreglo elementos, minimo y maximo dos numeros(Rojo)

--- EstadisticasTest.py
+++ EstadisticasTest.py
@@ -5,13 +5,13 @@ __author__ = 'cv.hernandez,vc.mendoza'
 
 class EstadisticasTest(TestCase):
     def test_Estadisticas(self):
-        self.assertEqual(Calculos().Estadisticas(""),[0,0,0], "arreglo elementos,minimo vacio")
+        self.assertEqual(Calculos().Estadisticas(""),[0,0,0], "arreglo elementos,minimo,maximo vacio")
 
     def test_Estadisticas_unNumero(self):
-        self.assertEqual(Calculos().Estadisticas("5"),[1,5,5], "arreglo elementos,minimo un elemento")
+        self.assertEqual(Calculos().Estadisticas("5"),[1,5,5], "arreglo elementos,minimo,maximo un elemento")
 
     def test_Estadisticas_dosNumeros(self):
-        self.assertEqual(Calculos().Estadisticas("5,2"),[2,2], "arreglo elementos,minimo dos elementos")
+        self.assertEqual(Calculos().Estadisticas("5,2"),[2,2,5], "arreglo elementos,minimo,maximo dos elementos")
 
     def test_Estadisticas_Numneros(self):
         self.assertEqual(Calculos().Estadisticas("5,2,11,6,3"),[5,2], "arreglo elementos,minimo N elementos")
