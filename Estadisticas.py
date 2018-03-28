917194122a992bc8956bd2d5cff4bdcca15e3bea
vivimendoza16 <vc.mendozab@uniandes.edu.co>
2018-03-28 11:32:40 -0500

    Paso 4. Arreglo elementos con dos numeros (Verde)

--- Estadisticas.py
+++ Estadisticas.py
@@ -5,5 +5,7 @@ class Calculos:
     def Estadisticas (self,cadena):
         if cadena == "":
             return ([0])
+        elif "," in cadena:
+            return([len((int(cadena[0]), int(cadena[2])))])
         else:
             return([len(cadena)])
